3cb7f7146bc7a0741ade673e0b75a2c129f76b23
Stephanie Nemeth <traumverloren@github.com>
2021-07-22 11:41:54 +0200

    store kv pair

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+  let mut arguments = std::env::args().skip(1);
+  let key = arguments.next().unwrap();
+  let value = arguments.next().unwrap();
+  println!("Key: {} Value: {}", key, value);
+  let write_results = write_database(key, value);
+  match write_results {
+    Ok(()) => {
+      println!("It worked");
+    }
+    Err(the_error) => {
+      println!("We got an error{}", the_error);
+    }
+  }
+}
+
+fn write_database(key: String, value: String) -> Result<(), std::io::Error> {
+  let contents = format!("{} {}", key, value);
+  std::fs::write("kv.db", contents)
 }
